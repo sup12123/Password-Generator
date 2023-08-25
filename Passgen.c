a9b740a0dff3e986bcb139391de3e54d34e2f060
sup12123 <95585537+sup12123@users.noreply.github.com>
2023-08-25 09:11:07 -0400

    Create Passgen.c

---
+++ Passgen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+
+int passlength;
+
+printf ("how many charecters must the password be? /n");
+  scanf ("%d", &passlength);
+
+
+  
+
+char *randstring(int length) {    
+    static int mySeed = 25011984;
+    char *string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+    size_t stringLen = strlen(string);        
+    char *randomString = NULL;
+
+    srand(time(NULL) * length + ++mySeed);
+
+    if (length < 1) {
+        length = 1;
+    }
+
+    randomString = malloc(sizeof(char) * (length +1));
+
+    if (randomString) {
+        short key = 0;
+
+        for (int n = 0;n < length;n++) {            
+            key = rand() % stringLen;          
+            randomString[n] = string[key];
+        }
+
+        randomString[length] = '\0';
+
+        return randomString;        
+    }
+    else {
+        printf("No memory");
+        exit(1);
+    }
+}
